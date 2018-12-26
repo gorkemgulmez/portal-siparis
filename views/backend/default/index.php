bc073363df55674f8921352a1ea99f6456398543
gorkemgulmez <gorkemgulmez7@gmail.com>
2018-12-27 00:26:27 +0300

    admin sayfası tablo eklenmesi

--- views/backend/default/index.php
+++ views/backend/default/index.php
@@ -2,11 +2,52 @@
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Kou Osl Yii2 App</h1>
+      <table class="blueTable">
+        <thead>
+          <tr>
+            <th>Sipariş No</th>
+            <th>Kullanıcı</th>
+            <th>&Uuml;r&uuml;n No</th>
+            <th>Durumu</th>
+            <th>Alım Tarih</th>
+            <th>İşlemler</th>
+          </tr>
+        </thead>
 
-        <p class="lead">Örnek uygulamayı başarılı bir şekilde çalıştırdınız.</p>
-
-        <p><a class="btn btn-lg btn-success" href="#">Modüller ve konfürgasyon!</a></p>
+        <tbody>
+          <tr>
+            <td>cell1_1</td>
+            <td>cell2_1</td>
+            <td>cell3_1</td>
+            <td>cell4_1</td>
+            <td>cell5_1</td>
+            <td>cell6_1</td>
+          </tr>
+          <tr>
+            <td>cell1_2</td>
+            <td>cell2_2</td>
+            <td>cell3_2</td>
+            <td>cell4_2</td>
+            <td>cell5_2</td>
+            <td>cell6_2</td>
+          </tr>
+          <tr>
+            <td>cell1_3</td>
+            <td>cell2_3</td>
+            <td>cell3_3</td>
+            <td>cell4_3</td>
+            <td>cell5_3</td>
+            <td>cell6_3</td>
+          </tr>
+          <tr>
+            <td>cell1_4</td>
+            <td>cell2_4</td>
+            <td>cell3_4</td>
+            <td>cell4_4</td>
+            <td>cell5_4</td>
+            <td>cell6_4</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-
 </div>
