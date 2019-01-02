75b667526124d60b9a974e189822d1628bbf872d
gorkemgulmez <gorkemgulmez@github.com>
2019-01-02 16:37:05 +0300

    siparis arayüz hazırlanma

--- views/frontend/default/index.php
+++ views/frontend/default/index.php
@@ -1,27 +1,44 @@
-<?= $this->title = 'Modül çalışıyor.'; ?>
+<?= $this->title = 'Sipariş Sayfası.'; ?>
+<p> Başlamadan Önce Notları Okuyun </p>
+
+<style type="text/css">
+.glyphicon {
+    font-size: 100px;
+    text-align: right;
+}
+
+span {
+    margin-bottom: 20px;
+}
+
+</style>
+
 <div class="site-index">
-
     <div class="jumbotron">
-        <h1>Sipariş</h1>
 
-        <div class="card" style="width: 18rem;">
-          <img class="card-img-top" src="" alt="Tepsi">
-          <div class="card-body">
-            Tepsi 1
-          </div>
-        </div>
-        <div class="card" style="width: 18rem;">
-          <img class="card-img-top" src="" alt="Tepsi">
-          <div class="card-body">
-            Tepsi 2
-          </div>
-        </div>
-        <div class="card" style="width: 18rem;">
-          <img class="card-img-top" src="" alt="Tepsi">
-          <div class="card-body">
-            Tepsi 3
-          </div>
+        <h2>Siparişleriniz</h2>
+        <div class="container">
+
+            <div class="row">
+                <div class="col-xs-6 col-md-3" style=" padding-top: 20px;">
+                    <img alt="Tepsi" class="img-thumbnail" src="https://img-trendyol.mncdn.com//Assets/ProductImages/33642/7131700503850_1_org.jpg" >
+                    <span >Sipariş No</span>
+                </div>
+
+                <div class="col-xs-6 col-md-5"> </div>
+
+                <div class="col-xs-6 col-md-4" >
+                    <div class="container">
+                        <span class="glyphicon glyphicon-eye-open"></span>
+                    </div>
+                    <div class="container">
+                        <span> Sipariş Adı: Tepsi </span>
+                    </div>
+                </div>
+            </div>
+
+
+
         </div>
     </div>
-
 </div>
