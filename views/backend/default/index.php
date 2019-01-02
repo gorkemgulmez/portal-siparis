60c6a1939da6d0f37cb0e776b0b5d163208b9e18
gorkemgulmez <gorkemgulmez@github.com>
2019-01-02 23:03:03 +0300

    backend açılır ekran ayarlaması

--- views/backend/default/index.php
+++ views/backend/default/index.php
@@ -13,29 +13,103 @@
     padding-right: 15px;
     padding-bottom: 25px;
   }
+
+  .close-button {
+      float: right;
+  }
+
+  .tableI {
+      background-color: #28d18f;
+      font-size: 12px;
+      text-align: left;
+  }
+
+  #overlay {
+      position: fixed;
+        display: none;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: rgba(0,0,0,0.5);
+        z-index: 2;
+        cursor: pointer;
+  }
+
+  #card-detail {
+      position: absolute;
+        top: 50%;
+        left: 50%;
+        font-size: 50px;
+        color: white;
+        transform: translate(-50%,-50%);
+        -ms-transform: translate(-50%,-50%);
+  }
 </style>
 
 <div class="site-index">
     <div class="jumbotron">
 
-      <form class="formC">
-        <span style="padding-right: 10px;">Sipariş Ara:</span>
-        <input type="search" placeholder="Sipariş No">
-        <input type="submit" value="Ara">
-      </form>
+        <div id="overlay" >
+            <div id="card-detail" style="background-color: #72bd34">
+                <div class="card text-center" style="margin: 30px" >
+                  <div class="card-header" >
+                    <span style="text-align: center;margin-left: 64px;">Sipariş Adı</span>
+                    <span class="btn close-button" onclick="closeCard()" >X</span>
+                  </div>
+                  <div class="card-body">
+                    <table class="table tableI">
+                        <tbody>
+                            <tr>
+                                <td style="text-align: left;">Sipariş Numarası</td>
+                                <td>1</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align: left;">Kullanıcı</td>
+                                <td>1</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align: left;">Ürün No</td>
+                                <td>1</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align: left;">Durumu</td>
+                                <td>1</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align: left;">Alım Tarihi</td>
+                                <td>1</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                  </div>
+                  <div class="card-footer btn" style="color: #d51919;">
+                    Siparişi İptal Et
+                  </div>
+                </div>
+            </div>
+        </div>
 
-      <table class="table">
+        <form class="formC">
+            <span style="padding-right: 10px;">Sipariş Ara:</span>
+            <input type="search" placeholder="Sipariş No">
+            <input type="submit" value="Ara">
+        </form>
 
-        <thead>
-          <tr>
-            <th scope="col">Sipariş No</th>
-            <th scope="col">Kullanıcı</th>
-            <th scope="col">&Uuml;r&uuml;n No</th>
-            <th scope="col">Durumu</th>
-            <th scope="col">Alım Tarih</th>
-            <th scope="col">İşlemler</th>
-          </tr>
-        </thead>
+        <table class="table">
+
+            <thead>
+                <tr>
+                    <th scope="col">Sipariş No</th>
+                    <th scope="col">Kullanıcı</th>
+                    <th scope="col">&Uuml;r&uuml;n No</th>
+                    <th scope="col">Durumu</th>
+                    <th scope="col">Alım Tarih</th>
+                    <th scope="col">İşlemler</th>
+                </tr>
+            </thead>
 
         <tbody>
           <tr>
@@ -45,9 +119,8 @@
             <td>İptal</td>
             <td>12.12.2018</td>
             <td>
-              <span class="glyphicon glyphicon-remove"></span>
-              <span class="glyphicon glyphicon-search"></span>
-              <span class="glyphicon glyphicon-cog"></span>
+                <span onclick="showCard()" class="glyphicon glyphicon-eye-open"></span>
+                <span class="glyphicon glyphicon-remove"></span>
             </td>
           </tr>
           <tr>
@@ -57,9 +130,8 @@
             <td>Yolda</td>
             <td>12.12.2019</td>
             <td>
-              <span class="glyphicon glyphicon-remove"></span>
-              <span class="glyphicon glyphicon-search"></span>
-              <span class="glyphicon glyphicon-cog"></span>
+                <span onclick="showCard()" class="glyphicon glyphicon-eye-open"></span>
+                <span class="glyphicon glyphicon-remove"></span>
             </td>
           </tr>
           <tr>
@@ -69,9 +141,8 @@
             <td>cell4_3</td>
             <td>cell5_3</td>
             <td>
-              <span class="glyphicon glyphicon-remove"></span>
-              <span class="glyphicon glyphicon-search"></span>
-              <span class="glyphicon glyphicon-cog"></span>
+                <span onclick="showCard()" class="glyphicon glyphicon-eye-open"></span>
+                <span class="glyphicon glyphicon-remove"></span>
             </td>
           </tr>
           <tr>
@@ -81,9 +152,8 @@
             <td>cell4_4</td>
             <td>cell5_4</td>
             <td>
-              <span class="glyphicon glyphicon-remove"></span>
-              <span class="glyphicon glyphicon-search"></span>
-              <span class="glyphicon glyphicon-cog"></span>
+                <span onclick="showCard()" class="glyphicon glyphicon-eye-open"></span>
+                <span class="glyphicon glyphicon-remove"></span>
             </td>
           </tr>
           <tr>
@@ -93,9 +163,8 @@
             <td>cell4_1</td>
             <td>cell5_1</td>
             <td>
-              <span class="glyphicon glyphicon-remove"></span>
-              <span class="glyphicon glyphicon-search"></span>
-              <span class="glyphicon glyphicon-cog"></span>
+                <span onclick="showCard()" class="glyphicon glyphicon-eye-open"></span>
+                <span class="glyphicon glyphicon-remove"></span>
             </td>
           </tr>
           <tr>
@@ -105,9 +174,8 @@
             <td>cell4_2</td>
             <td>cell5_2</td>
             <td>
-              <span class="glyphicon glyphicon-remove"></span>
-              <span class="glyphicon glyphicon-search"></span>
-              <span class="glyphicon glyphicon-cog"></span>
+                <span onclick="showCard()" class="glyphicon glyphicon-eye-open"></span>
+                <span class="glyphicon glyphicon-remove"></span>
             </td>
           </tr>
           <tr>
@@ -117,9 +185,8 @@
             <td>cell4_3</td>
             <td>cell5_3</td>
             <td>
-              <span class="glyphicon glyphicon-remove"></span>
-              <span class="glyphicon glyphicon-search"></span>
-              <span class="glyphicon glyphicon-cog"></span>
+                <span onclick="showCard()" class="glyphicon glyphicon-eye-open"></span>
+                <span class="glyphicon glyphicon-remove"></span>
             </td>
           </tr>
           <tr>
@@ -129,9 +196,8 @@
             <td>cell4_4</td>
             <td>cell5_4</td>
             <td>
-              <span class="glyphicon glyphicon-remove"></span>
-              <span class="glyphicon glyphicon-search"></span>
-              <span class="glyphicon glyphicon-cog"></span>
+                <span onclick="showCard()" class="glyphicon glyphicon-eye-open"></span>
+                <span class="glyphicon glyphicon-remove"></span>
             </td>
           </tr>
           <tr>
@@ -141,9 +207,8 @@
             <td>cell4_1</td>
             <td>cell5_1</td>
             <td>
-              <span class="glyphicon glyphicon-remove"></span>
-              <span class="glyphicon glyphicon-search"></span>
-              <span class="glyphicon glyphicon-cog"></span>
+                <span onclick="showCard()" class="glyphicon glyphicon-eye-open"></span>
+                <span class="glyphicon glyphicon-remove"></span>
             </td>
           </tr>
           <tr>
@@ -153,9 +218,8 @@
             <td>cell4_2</td>
             <td>cell5_2</td>
             <td>
-              <span class="glyphicon glyphicon-remove"></span>
-              <span class="glyphicon glyphicon-search"></span>
-              <span class="glyphicon glyphicon-cog"></span>
+                <span onclick="showCard()" class="glyphicon glyphicon-eye-open"></span>
+                <span class="glyphicon glyphicon-remove"></span>
             </td>
           </tr>
           <tr>
@@ -165,9 +229,8 @@
             <td>cell4_3</td>
             <td>cell5_3</td>
             <td>
-              <span class="glyphicon glyphicon-remove"></span>
-              <span class="glyphicon glyphicon-search"></span>
-              <span class="glyphicon glyphicon-cog"></span>
+                <span onclick="showCard()" class="glyphicon glyphicon-eye-open"></span>
+                <span class="glyphicon glyphicon-remove"></span>
             </td>
           </tr>
           <tr>
@@ -177,9 +240,8 @@
             <td>cell4_4</td>
             <td>cell5_4</td>
             <td>
-              <span class="glyphicon glyphicon-remove"></span>
-              <span class="glyphicon glyphicon-search"></span>
-              <span class="glyphicon glyphicon-cog"></span>
+                <span onclick="showCard()" class="glyphicon glyphicon-eye-open"></span>
+                <span class="glyphicon glyphicon-remove"></span>
             </td>
           </tr>
           <tr>
@@ -189,9 +251,8 @@
             <td>cell4_1</td>
             <td>cell5_1</td>
             <td>
-              <span class="glyphicon glyphicon-remove"></span>
-              <span class="glyphicon glyphicon-search"></span>
-              <span class="glyphicon glyphicon-cog"></span>
+                <span onclick="showCard()" class="glyphicon glyphicon-eye-open"></span>
+                <span class="glyphicon glyphicon-remove"></span>
             </td>
           </tr>
           <tr>
@@ -201,9 +262,8 @@
             <td>cell4_2</td>
             <td>cell5_2</td>
             <td>
-              <span class="glyphicon glyphicon-remove"></span>
-              <span class="glyphicon glyphicon-search"></span>
-              <span class="glyphicon glyphicon-cog"></span>
+                <span onclick="showCard()" class="glyphicon glyphicon-eye-open"></span>
+                <span class="glyphicon glyphicon-remove"></span>
             </td>
           </tr>
           <tr>
@@ -213,9 +273,8 @@
             <td>cell4_3</td>
             <td>cell5_3</td>
             <td>
-              <span class="glyphicon glyphicon-remove"></span>
-              <span class="glyphicon glyphicon-search"></span>
-              <span class="glyphicon glyphicon-cog"></span>
+                <span onclick="showCard()" class="glyphicon glyphicon-eye-open"></span>
+                <span class="glyphicon glyphicon-remove"></span>
             </td>
           </tr>
           <tr>
@@ -225,9 +284,8 @@
             <td>cell4_4</td>
             <td>cell5_4</td>
             <td>
-              <span class="glyphicon glyphicon-remove"></span>
-              <span class="glyphicon glyphicon-search"></span>
-              <span class="glyphicon glyphicon-cog"></span>
+                <span onclick="showCard()" class="glyphicon glyphicon-eye-open"></span>
+                <span class="glyphicon glyphicon-remove"></span>
             </td>
           </tr>
         </tbody>
@@ -235,3 +293,13 @@
       </table>
     </div>
 </div>
+
+<script type="text/javascript" >
+    function showCard() {
+        document.getElementById("overlay").style.display = "block";
+    }
+
+    function closeCard() {
+        document.getElementById("overlay").style.display = "none";
+    }
+</script>
