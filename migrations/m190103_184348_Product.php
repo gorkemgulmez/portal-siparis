f89081c8aed9acf8370ee538a8a41e20d4679406
gorkemgulmez <gorkemgulmez@github.com>
2019-01-04 14:42:59 +0300

    boş migration eklenmesi

---
+++ migrations/m190103_184348_Product.php
@@ -0,0 +1,22 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190103_184348_Product
+ */
+class m190103_184348_Product extends Migration
+{
+
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190103_184348_Product cannot be reverted.\n";
+
+        return false;
+    }
+}
