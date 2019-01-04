729414eef92b848f36a950038fe3cbb67350e8a7
gorkemgulmez <gorkemgulmez@github.com>
2019-01-04 14:44:13 +0300

    Widget icin frontend controller

--- controllers/frontend/DefaultController.php
+++ controllers/frontend/DefaultController.php
@@ -1,6 +1,19 @@
 <?php
 namespace kouosl\siparis\controllers\frontend;
+use kouosl\siparis\models\Orders;
 
+
+use yii\helpers\ArrayHelper;
+use Yii;
+use yii\base\InvalidParamException;
+use yii\web\BadRequestHttpException;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use kouosl\iletisim\models\ContactForm;
+use kouosl\iletisim\models\Tablo;
+use kouosl\iletisim\models\UploadForm;
+use yii\web\UploadedFile;
+use yii\filters\Cors;
 
 /**
  * Default controller for the `siparis` module
@@ -13,6 +26,17 @@ class DefaultController extends \kouosl\base\controllers\frontend\BaseController
      */
     public function actionIndex()
     {
-        return $this->render('_index');
+        $model = new Orders();
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            if($model->save()) {
+                Yii::$app->session->setFlash('success', 'Başarılı.');
+            }
+            else {
+                Yii::$app->session->setFlash('error', 'Hata!');
+            }
+            return $this->refresh();
+        } else {
+            return $this->render('_index', ['model' => $model]);
+        }
     }
 }
